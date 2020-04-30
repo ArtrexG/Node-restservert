c7721ba7519fa1c200afec34ba02c94d272e5b22
ArtrexG <animetallex@live.com>
2020-04-29 23:33:30 -0500

    Rest-server-Usuarios

--- server/config/config.js
+++ server/config/config.js
@@ -8,7 +8,7 @@ process.env.NODE_ENV = process.env.NODE_ENV|| 'dev';
 
 let urlDB;
 
-if(proces.env.NODE_ENV == 'dev'){
+if(process.env.NODE_ENV == 'dev'){
     urlDB ='mongodb://localhost:27017/database';
 }else{
     urlDB = "mongodb+srv://ARTREX:xbox3600A@cluster0-oyphd.mongodb.net/cafe"
